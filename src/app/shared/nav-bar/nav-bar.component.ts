863d9b2e95dd1205f10575454cda8a03f4e9fcc5
Bala L <balaji.lakkusamy@gmail.com>
2022-09-07 19:59:28 +0530

    added home page with test service

---
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styles: [
+    `
+    .spacer {
+      flex: 1 1 auto;
+    }
+    `
+  ]
+})
+export class NavBarComponent {
+}
